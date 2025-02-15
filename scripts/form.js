d19e893c5e6a99cdf07c6a9da79084fea1745e63
Alan Scalia <alan.sca07@gmail.com>
2025-02-15 00:06:58 -0300

    W05 Project final submission

--- scripts/form.js
+++ scripts/form.js
@@ -8,7 +8,6 @@ lastmod.innerHTML = 'Last Updated: ' + document.lastModified;
 
 
 // Array of Products
-
 const products = [
     {
         id: "fc-1888",
@@ -40,9 +39,39 @@ const products = [
 
 // Products select implementation
 
-const select = document.querySelector('#product-name');
+function getProducts(products) {
+    const select = document.querySelector('#product-name');
 
-products.forEach((product) => { const newOption = document.createElement('option'); select.appendChild(newOption).innerHTML = `<option value="${product.id}">${product.name.charAt(0).toUpperCase() + product.name.slice(1)}</option>`; })
+    products.forEach((product) => { const newOption = document.createElement('option'); select.appendChild(newOption).innerHTML = `<option value="${product.id}">${product.name.charAt(0).toUpperCase() + product.name.slice(1)}</option>`; })
+}
+
+if (document.querySelector('#product-name')) {
+    getProducts(products);
+}
 
 // Review counter
 
+function reviewCount() {
+    const postButton = document.querySelector('#post');
+    const rCounter = document.querySelector('#review-counter');
+    let reviewsStored = Number(localStorage.getItem('reviewCounter'));
+
+
+    console.log('reviews: ' + reviewsStored);
+
+    if (reviewsStored !== 0) {
+        rCounter.innerHTML = reviewsStored;
+    }
+    else {
+        rCounter.textContent = 'This is your first review!';
+    }
+
+    reviewsStored++
+    Number(localStorage.setItem('reviewCounter', reviewsStored));
+
+    console.log(reviewsStored);
+}
+
+if (document.querySelector('#confirmation')) {
+    reviewCount();
+}
